7434534d814e29b4234249c20f56fc8a93eab098
TeamSangai <teamsangaiwastaken@gmail.com>
2025-01-17 12:23:54 -0500

    just a few more issues with setting up power types.

--- src/main/java/net/mrgeoves/originsxtoughasnails/OriginsXToughAsNails.java
+++ src/main/java/net/mrgeoves/originsxtoughasnails/OriginsXToughAsNails.java
@@ -2,6 +2,7 @@ package net.mrgeoves.originsxtoughasnails;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.mrgeoves.originsxtoughasnails.power.type.OxTanPowerTypes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +15,7 @@ public class OriginsXToughAsNails implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	public void onInitialize() {
-		ModifyThirstExhaustion.register();
+		OxTanPowerTypes.register();
 	}
 
 }
